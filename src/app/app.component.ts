68b65083084fcb551a1049f562f77e429d12ff5f
MoritzMeinhardt <moritz.meinhardt@risiq.de>
2025-06-07 09:40:32 +0200

    initial component structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatSidenavContainer} from '@angular/material/sidenav';
+import {PersonListComponent} from './components/person-list/person-list.component';
+import {OverviewComponent} from './components/overview/overview.component';
+import {PersonDetailComponent} from './components/person-detail/person-detail.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavContainer, PersonListComponent, OverviewComponent, PersonDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
---
+++ src/app/components/person-detail/person-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-detail',
+  imports: [],
+  templateUrl: './person-detail.component.html',
+  styleUrl: './person-detail.component.less'
+})
+export class PersonDetailComponent {
+
+}
